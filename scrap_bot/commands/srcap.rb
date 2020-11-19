ff34fc282735e29720b513cee86c3b2ea83cb8a3
Misori-simon <misorisimon@gmail.com>
2020-11-19 13:11:20 +0100

    add commands scrap file -add commands to the scrap bot class

--- scrap_bot/commands/srcap.rb
+++ scrap_bot/commands/srcap.rb
@@ -1,1 +1,27 @@
-# something
+require_relative 'scraper'
+module GeoBot
+  module Commands
+    class Geo < SlackRubyBot::Commands::Base
+      command 'greet' do |client, data, _match|
+        client.say(channel: data.channel, text: 'hello')
+      end
+      command 'intro' do |client, data, _match|
+        client.say(channel: data.channel, text: BotInfo.new.about)
+      end
+      match /^How is the weather in (?<location>\w*)\?$/i do |client, data, match|
+        client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
+      end
+      match /^wiki (?<location>.*)$/i do |client, data, match|
+        client.say(channel: data.channel, text: Scraper.new.wiki(match[:location]))
+        # client.say(channel: data.channel, text: "The wiki in #{match[:location]} is nice.")
+      end
+
+      match /^buy (?<location>.*)$/i do |client, data, match|
+        item = Scraper.new.ebay(match[:location])
+        item = Scraper.new.group_to_string(item)
+        client.say(channel: data.channel, text: item)
+        # client.say(channel: data.channel, text: "The wiki in #{match[:location]} is nice.")
+      end
+    end
+  end
+end
