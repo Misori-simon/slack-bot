4d024328bdc5166093aaf22a863f96e50a8277d1
Misori-simon <misorisimon@gmail.com>
2020-11-19 13:08:14 +0100

    add a BotInfo class to bot_info.rb -add class to hold information about bot

---
+++ scrap_bot/commands/bot_info.rb
@@ -0,0 +1,17 @@
+class BotInfo
+  def name
+    'Name: Geobot'
+  end
+
+  def intro
+    'Intro: I am a bot that tells you the local time and weather of a city you enter'
+  end
+
+  def how_to
+    "How to use: type '@bot_name city' to get the local time and weather of the city"
+  end
+
+  def about
+    "#{name} \n#{intro} \n#{how_to}"
+  end
+end
